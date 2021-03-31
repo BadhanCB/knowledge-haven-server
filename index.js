0fe3f3d3178423f689719c948e70720a60e8001a
Badhan112 <badhancsesastc112@gmail.com>
2021-03-31 15:24:30 +0600

    connected to the mongodb

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 1712;
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.brit3.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(express.json());
+
+client.connect(err => {
+  const bookCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.BOOK_COLLECTION}`);
+  console.log('MongoDB database Connected');
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome to NodeJS & ExpressJS world');
